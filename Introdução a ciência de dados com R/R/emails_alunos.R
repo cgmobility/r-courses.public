392483784591b457504bfa4cb041f93453f3376e
Caio Gustavo Guimarães <caiogcg.mobilidade@gmail.com>
2024-11-01 13:25:02 -0300

    Add files via upload

--- Introdução a ciência de dados com R/R/emails_alunos.R
+++ Introdução a ciência de dados com R/R/emails_alunos.R
@@ -44,27 +44,26 @@ endcourse_email <- function(student_name){
     tags$div(
       style = 'background-color: #722E9C;width:100%;height:100px;display:flex;',
       tags$img(
-        src = 'https://icon.icepanel.io/Technology/svg/RStudio.svg',
+        src = 'https://raw.githubusercontent.com/cgmobility/r-courses.public/refs/heads/main/Introdu%C3%A7%C3%A3o%20a%20ci%C3%AAncia%20de%20dados%20com%20R/RStudio.png',
         style = '
         height: 60px;
         margin-left: 20px;
         margin-top: auto;
         margin-bottom: auto;'
       ),
-      tags$h3(
+      tags$h2(
         'INTRODUÇÃO À CIÊNCIA DE DADOS COM R',
-        style = 'text-align:center;color: white;margin-top:auto;
-        margin-left: auto;margin-bottom:auto;'
+        style = 'text-align:center;color: white;margin:auto;'
       ),
-      tags$img(
-        src = 'https://www.unichristus.edu.br/wp-content/uploads/2022/10/Logo-Unichristus-em-Branco.png',
-        style = '
-        height: 30px;
-        margin-left: auto;
-        margin-top: auto;
-        margin-bottom: auto;
-        margin-right: 20px;'
-      )
+      # tags$img(
+      #   src = 'https://www.unichristus.edu.br/wp-content/uploads/2022/10/Logo-Unichristus-em-Branco.png',
+      #   style = '
+      #   height: 30px;
+      #   margin-left: auto;
+      #   margin-top: auto;
+      #   margin-bottom: auto;
+      #   margin-right: 20px;'
+      # )
     ),
     tags$div(
       id = 'email-body',
@@ -73,13 +72,13 @@ endcourse_email <- function(student_name){
         paste0('Olá ',first_name,'!'),
         style = 'margin-bottom:20px;text-align:center;'
       ),
-      tags$h5(
+      tags$h4(
         'Gostaríamos de agradecer pela sua participação no curso ciencia de dados com R! Esperamos que ele tenha ajudado você a destravar novas habilidades na linguagem e a explorar o potencial que o R oferece para análise de dados e visualização.'
       ),
-      tags$h5(
+      tags$h4(
         'O R é uma ferramenta poderosa e versátil, capaz de transformar grandes volumes de dados em informações claras e estratégicas. Nossa jornada até aqui foi só o começo, e esperamos que você se sinta mais preparado(a) para explorar essa linguagem incrível.'
       ),
-      tags$h5(
+      tags$h4(
         'Informamos que todo o material do curso estará permanentemente disponível no drive do curso'
       ),
       tags$div(
@@ -99,7 +98,7 @@ endcourse_email <- function(student_name){
           )
         )
       ),
-      tags$h5(
+      tags$h4(
         'Para nós, seu feedback é essencial! Pedimos que você compartilhe suas impressões sobre o curso na pesquisa de avaliação abaixo. Suas respostas nos ajudam a continuar aprimorando o conteúdo e a experiência:'
       ),
       tags$div(
@@ -119,7 +118,7 @@ endcourse_email <- function(student_name){
           )
         )
       ),
-      tags$h5(
+      tags$h4(
         'Além disso, não deixe de acompanhar nossos perfis nas redes sociais para mais dicas, novidades e, claro, para nos mantermos conectados:'
       ),
       tags$div(
@@ -180,7 +179,7 @@ endcourse_email <- function(student_name){
         )
         
       ),
-      tags$h5(
+      tags$h4(
         'E qualquer dúvida ou sugestão, estamos disponíveis nos emails abaixo:'
       ),
       tags$div(
@@ -195,7 +194,7 @@ endcourse_email <- function(student_name){
           ),
           tags$p(
             'Caio Guimarães',
-            style = 'margin-left: 5px; color: black;'
+            style = 'margin-left: 5px; color: black;margin-top: auto;margin-bottom: auto;'
           )
         ),
         tags$a(
@@ -209,15 +208,15 @@ endcourse_email <- function(student_name){
           ),
           tags$p(
             'Nelson Quesado',
-            style = 'margin-left: 5px; color: black;'
+            style = 'margin-left: 5px; color: black;margin-top: auto;margin-bottom: auto;'
           )
         )
         
       ),
-      tags$h5(
+      tags$h4(
         'Muito obrigado mais uma vez, e esperamos que o R continue sendo uma ferramenta valiosa para sua jornada. Sucesso e nos vemos em breve!'
       ),
-      tags$h5(
+      tags$h4(
         'Um grande abraço!',
         tags$br(),
         'Caio Gustavo Guimarães e Nelson Quesado'
@@ -229,30 +228,46 @@ endcourse_email <- function(student_name){
       tags$h5(
         style = 'text-align:center;',
         'Este email foi gerado e enviado utilizando o R.',
-        tags$a('Veja como.')
+        tags$a(
+          href = 'https://github.com/cgmobility/r-courses.public/blob/main/Introdu%C3%A7%C3%A3o%20a%20ci%C3%AAncia%20de%20dados%20com%20R/R/emails_alunos.R',
+          target = '_blank',
+          'Veja como.'
+        )
       )
     )
   ) %>% return()
 }
 
 
+
+
 # Envio usando gmailR -----------------------------------------------------
 
-library(shiny)
+
+path_old <- paste0(getwd(),'/secrets/client_secret_987464996782-sjcsbd0erhmcf986tokhppple499p929.apps.googleusercontent.com.json')
+fs::file_copy(path_old, fs::dir_create(rappdirs::user_data_dir("gmailr"), recurse = TRUE))
 
 
+Sys.setenv(GMAILR_OAUTH_CLIENT=paste0(getwd(),'/secrets/client_secret_987464996782-sjcsbd0erhmcf986tokhppple499p929.apps.googleusercontent.com.json'))
 
-ui <- fluidPage(
-  endcourse_email('Nelson Quesado')
-)
+gm_auth_configure()
 
-server <- function(input, output, session) {
+gm_oauth_client()
+
+for (i in 2:nrow(sheet)) {
+  
+  email_dest <- sheet$`Endereço de e-mail`[i]
+  student_nm <- sheet$`Nome completo`[i]
+  
+  mail_content <- gm_mime() %>% 
+    gm_to(c(email_dest,'nquesado@gmail.com')) %>% 
+    gm_from('caiogcg.mobilidade@gmail.com') %>% 
+    gm_subject('Obrigado por explorar o mundo do R com a gente!') %>% 
+    gm_html_body(as.character(endcourse_email(student_nm)))
+  
+  gm_send_message(mail_content)
   
 }
-
-runApp(shinyApp(ui, server),launch.browser = T)
-
-
 
 
 
